9bed0d6b56c1be6cc7a35f472a75001a6494aad3
verdaux <xachineon@gmail.com>
2020-04-01 19:17:24 +0530

    Adapter package added

---
+++ src/com/adapter/ducks/Drone.java
@@ -0,0 +1,7 @@
+package com.adapter.ducks;
+
+public interface Drone {
+	public void beep();
+	public void spin_rotors();
+	public void take_off();
+}
---
+++ src/com/adapter/ducks/Duck.java
@@ -0,0 +1,6 @@
+package com.adapter.ducks;
+
+public interface Duck {
+	public void quack();
+	public void fly();
+}
